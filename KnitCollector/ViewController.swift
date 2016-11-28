28bf4bc044cfa9b737a55f6f1ec65a58cadab197
Dan Waseen <dan.waseen1@gmail.com>
2016-11-28 10:47:21 -0600

    Initial Commit

---
+++ KnitCollector/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KnitCollector
+//
+//  Created by Dan Waseen on 11/28/16.
+//  Copyright © 2016 RevWon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
